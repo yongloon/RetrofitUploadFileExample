435085fd18b1db861a847474b3de6e52b82085dd
HEXA-KRISTOFERN\UF-Choon.Shyan <kristofer.ng@hexainitiative.com>
2017-04-03 11:23:06 +0800

    Add Check permission

--- app/src/main/java/com/questdot/retrofituploadfileexample/MainActivity.java
+++ app/src/main/java/com/questdot/retrofituploadfileexample/MainActivity.java
@@ -1,106 +1,114 @@
 package com.questdot.retrofituploadfileexample;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-                import android.provider.MediaStore;
-                import android.support.design.widget.Snackbar;
-                import android.support.v7.app.AppCompatActivity;
-                import android.support.v7.widget.Toolbar;
-                import android.view.View;
-                import android.widget.Button;
-                import android.widget.ImageView;
-                import android.widget.Toast;
+import android.provider.MediaStore;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-                import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Picasso;
 
-                import java.io.File;
+import java.io.File;
 
-                import okhttp3.MediaType;
-                import okhttp3.MultipartBody;
-                import okhttp3.RequestBody;
-                import retrofit2.Call;
-                import retrofit2.Callback;
-                import retrofit2.Response;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
-                public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
 
+    ImageView imageView;
+    String imagePath;
+    Toolbar toolbar;
 
-                    ImageView imageView;
-                    String imagePath;
-                    Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-                    @Override
-                    protected void onCreate(Bundle savedInstanceState) {
-                        super.onCreate(savedInstanceState);
-                        setContentView(R.layout.activity_main);
-
-
-                        toolbar = (Toolbar) findViewById(R.id.toolbar);
-                        setSupportActionBar(toolbar);
-
-
-                        imageView = (ImageView) findViewById(R.id.imageView);
-
-                        Button button = (Button) findViewById(R.id.fab);
-
-
-                        button.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-
-                                if(imagePath!=null)
-                                    uploadImage();
-                                else
-                                    Toast.makeText(getApplicationContext(),"Please select image", Toast.LENGTH_LONG).show();
-
-                            }
-                        });
-                    }
+        verifyStoragePermissions(MainActivity.this);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
 
-                    private void uploadImage() {
+        imageView = (ImageView) findViewById(R.id.imageView);
 
-                        final ProgressDialog progressDialog;
-                        progressDialog = new ProgressDialog(MainActivity.this);
-                        progressDialog.setMessage("loading...");
-                        progressDialog.show();
+        Button button = (Button) findViewById(R.id.fab);
 
-                        FileApi service = RetroClient.getApiService();
 
-                        File file = new File(imagePath);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-                        RequestBody requestFile = RequestBody.create(MediaType.parse("multipart/form-data"), file);
+                if(imagePath!=null)
+                    uploadImage();
+                else
+                    Toast.makeText(getApplicationContext(),"Please select image", Toast.LENGTH_LONG).show();
 
-                        MultipartBody.Part body =
-                                MultipartBody.Part.createFormData("uploaded_file", file.getName(), requestFile);
+            }
+        });
+    }
 
-                        Call<Respond> resultCall = service.uploadImage(body);
+    private void uploadImage() {
 
-                        resultCall.enqueue(new Callback<Respond>() {
-                            @Override
-                            public void onResponse(Call<Respond> call, Response<Respond> response) {
+        final ProgressDialog progressDialog;
+        progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setMessage("loading...");
+        progressDialog.show();
 
-                                progressDialog.dismiss();
+        FileApi service = RetroClient.getApiService();
 
-                                // Response Success or Fail
-                                if (response.isSuccessful()) {
-                                    if (response.body().getError()==true)
+        File file = new File(imagePath);
 
-                                        Toast.makeText(getApplicationContext(),response.body().getMessage(),Toast.LENGTH_LONG).show();
+        RequestBody requestFile = RequestBody.create(MediaType.parse("multipart/form-data"), file);
 
-                                    else
-                                        Toast.makeText(getApplicationContext(),response.body().getMessage(),Toast.LENGTH_LONG).show();
+        MultipartBody.Part body =
+                MultipartBody.Part.createFormData("uploaded_file", file.getName(), requestFile);
 
-                                } else {
-                                    Toast.makeText(getApplicationContext(),response.body().getMessage(),Toast.LENGTH_LONG).show();
-                                }
+        Call<Respond> resultCall = service.uploadImage(body);
 
-                                imageView.setImageDrawable(null);
-                                imagePath = null;
+        resultCall.enqueue(new Callback<Respond>() {
+            @Override
+            public void onResponse(Call<Respond> call, Response<Respond> response) {
+
+                progressDialog.dismiss();
+
+                // Response Success or Fail
+                if (response.isSuccessful()) {
+                    if (response.body().getError()==true)
+
+                        Toast.makeText(getApplicationContext(),response.body().getMessage(),Toast.LENGTH_LONG).show();
+
+                    else
+                        Toast.makeText(getApplicationContext(),response.body().getMessage(),Toast.LENGTH_LONG).show();
+
+                } else {
+                    Toast.makeText(getApplicationContext(),response.body().getMessage(),Toast.LENGTH_LONG).show();
+                }
+
+                imageView.setImageDrawable(null);
+                imagePath = null;
 
             }
 
@@ -113,12 +121,12 @@ import android.os.Bundle;
 
 
     public void showImagePopup(View view) {
-            final Intent galleryIntent = new Intent();
-            galleryIntent.setType("image/*");
-            galleryIntent.setAction(Intent.ACTION_PICK);
+        final Intent galleryIntent = new Intent();
+        galleryIntent.setType("image/*");
+        galleryIntent.setAction(Intent.ACTION_PICK);
 
-            final Intent chooserIntent = Intent.createChooser(galleryIntent, "Choose image");
-            startActivityForResult(chooserIntent, 100);
+        final Intent chooserIntent = Intent.createChooser(galleryIntent, "Choose image");
+        startActivityForResult(chooserIntent, 100);
 
     }
 
@@ -131,6 +139,10 @@ import android.os.Bundle;
                 Toast.makeText(getApplicationContext(),"Unable to pick image",Toast.LENGTH_LONG).show();
                 return;
             }
+
+            Uri imageUri = data.getData();
+            imageView.setImageURI(imageUri);
+         /*
             Uri selectedImageUri = data.getData();
             String[] filePathColumn = {MediaStore.Images.Media.DATA};
 
@@ -139,19 +151,44 @@ import android.os.Bundle;
             if (cursor != null) {
                 cursor.moveToFirst();
 
-                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-                imagePath = cursor.getString(columnIndex);
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);*/
+            imagePath = getRealPathFromURI(imageUri);
 
-                Picasso.with(getApplicationContext()).load(new File(imagePath))
-                        .into(imageView);
+            //    Picasso.with(getApplicationContext()).load(new File(imagePath))
+            //            .into(imageView);
 
-                Toast.makeText(getApplicationContext(),"Please reselect your image",Toast.LENGTH_LONG).show();
-                cursor.close();
+            //   Toast.makeText(getApplicationContext(),"Please reselect your image",Toast.LENGTH_LONG).show();
+           /*     cursor.close();
 
             } else {
 
                 Toast.makeText(getApplicationContext(),"Unable to load image",Toast.LENGTH_LONG).show();
-            }
+            }*/
+        }
+    }
+
+    private String getRealPathFromURI(Uri contentUri) {
+        String[] proj = { MediaStore.Images.Media.DATA };
+        CursorLoader loader = new CursorLoader(getApplicationContext(), contentUri, proj, null, null, null);
+        Cursor cursor = loader.loadInBackground();
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        String result = cursor.getString(column_index);
+        cursor.close();
+        return result;
+    }
+
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
         }
     }
 
